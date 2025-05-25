be412053f5c9fcd867ac74ae83502afe323122e3
Ar0gen <a344034406@gmail.com>
2025-05-25 13:09:34 +0800

    message 10: First model migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.template.loader import render_to_string
+from lists.models import Item
 
 class HomePageTest(TestCase):
     def test_use_home_template(self):
@@ -12,6 +13,7 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn("A new list item", response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
@@ -20,4 +22,23 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 # Create your tests here.
