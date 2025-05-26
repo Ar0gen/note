146ebd4267bc749afa428f6b60dd1124475ca987
Ar0gen <a344034406@gmail.com>
2025-05-26 13:01:38 +0800

    message 13: refractor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,9 +2,12 @@ import time
 import unittest
 
 from selenium import webdriver
+from selenium.common import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from django.test import  LiveServerTestCase
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -12,6 +15,19 @@ class NewVisitorTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return  # (4)
+            except (AssertionError, WebDriverException) as e:  # (5)
+                if time.time() - start_time > MAX_WAIT:  # (6)
+                    raise e  # (6)
+                time.sleep(0.5)  # (5)
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element(By.ID, "id_list_table")
@@ -39,19 +55,17 @@ class NewVisitorTest(LiveServerTestCase):
         # 他按了回车键键后，页面更新了
         # 待功事项表格中显示了“1：Buy flowers"
         inputbox.send_keys(Keys.ENTER)# (3)
-        time.sleep(1)   # (4)
-        self.check_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('1: Buy flowers')
 
         # 页面中又显示了一个文本输入框，可以输入其他待办事项
         # 他输入了“Give a gift to Lisi"
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
-        self.check_for_row_in_list_table('1: Buy flowers')
-        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
 
         # 张三想知道这个网站是否会记住她的清单
         # 他看到网站为他生成了一个唯一的URL
